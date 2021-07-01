3e06280f2abdeaa2675315357c065f101bde53f7
Defluxit <astroghost1@gmail.com>
2021-07-01 02:12:14 -0500

    Added to script.js, modified style.css, updated index.html

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function chngimg(n){
-if(n){
-document.images['fst'].src="nobuttn.gif";
-document.images['sec'].src="blnkbut.gif";
-}else{
-document.images['sec'].src="nobuttn.gif";
-document.images['fst'].src="blnkbut.gif";
-}}
+// Create a JavaScript Date object for the current date and time, and set the desired options.
+var date = new Date(),
+     options = { weekday: "long", month: "long", day: "numeric", year: "numeric" };
+
+// Convert to locale string and add a locale and the options
+date = date.toLocaleString( "en-US", options );
+ 
+// Output the date to the above HTML element
+document.getElementById("msg").innerHTML = date;
