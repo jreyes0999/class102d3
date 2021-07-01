0a04f21fa9933a746ef17801faa2f8c6cb18973e
Defluxit <astroghost1@gmail.com>
2021-07-01 04:26:30 -0500

    Added to script.js, fixed index.html to read script.js, added to style.css

--- script.js
+++ script.js
@@ -1,9 +1,73 @@
-// Create a JavaScript Date object for the current date and time, and set the desired options.
-var date = new Date(),
-     options = { weekday: "long", month: "long", day: "numeric", year: "numeric" };
+function closeCurrentTab(){
+     var conf=confirm("Are you sure you want to leave my awesome webpage?");
+     if(conf==true){
+          close();
+     }
+}
 
-// Convert to locale string and add a locale and the options
-date = date.toLocaleString( "en-US", options );
+function startTime() {
+     var today = new Date();
+     var hr = today.getHours();
+     var min = today.getMinutes();
+     var sec = today.getSeconds();
+     ap = (hr < 12) ? "AM" : "PM";
+     hr = (hr == 0) ? 12 : hr;
+     hr = (hr > 12) ? hr - 12 : hr;
+     //Add a zero in front of numbers<10
+     hr = checkTime(hr);
+     min = checkTime(min);
+     sec = checkTime(sec);
+     document.getElementById("clock").innerHTML = hr + ":" + min + ":" + sec + " " + ap;
+     
+     var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+     var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+     var curWeekDay = days[today.getDay()];
+     var curDay = today.getDate();
+     var curMonth = months[today.getMonth()];
+     var curYear = today.getFullYear();
+     var date = curWeekDay+", "+curDay+" "+curMonth+" "+curYear;
+     document.getElementById("date").innerHTML = date;
+     
+     var time = setTimeout(function(){ startTime() }, 500);
+ }
+ function checkTime(i) {
+     if (i < 10) {
+         i = "0" + i;
+     }
+     return i;
+ }
  
-// Output the date to the above HTML element
-document.getElementById("msg").innerHTML = date;
+ var d=new Date()
+ theDay=d.getDay()
+ switch (theDay)
+ {
+ case 1:
+   document.write("Oh no, it's Monday.")
+   break
+ case 2:
+   document.write("Just another Tuesday.")
+   break
+ case 3:
+   document.write("HUMP DAY!")
+   break
+ case 4:
+   document.write("So close to Friday!")
+   break
+ case 5:
+   document.write("It's finally Friday!")
+   break
+ case 6:
+   document.write("I love Saturdays.")
+   break
+ case 0:
+   document.write("Lazy Sunday.")
+   break
+ default:
+   document.write("I'm looking forward to this weekend!")
+ }
+
+ if (confirm("WELCOME!")) {
+    txt = "Thanks! (Proceed)";
+  } else {
+    txt = "No Thanks! (Proceed Anyway)";
+  }
